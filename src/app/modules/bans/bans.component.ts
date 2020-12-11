18ba6f97caa1d7055e2c796e973bb72f98af8f6c
Quentin Fornasier <q.fornasier@hi.org>
2020-12-11 19:31:02 +0100

    sanctions

--- src/app/modules/bans/bans.component.ts
+++ src/app/modules/bans/bans.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bans',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BansComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private titleService:Title) {
+    this.titleService.setTitle('Over2Craft - Liste des sanctions  (bans)');
+  }
   ngOnInit(): void {
   }
 
