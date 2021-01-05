cc5593a3b34ca918dfe2a69d5ffcd19fa000d3a9
Quentin Fornasier <q.fornasier@hi.org>
2021-01-05 22:31:16 +0100

    discord login

--- src/app/modules/donnations/donnations.component.ts
+++ src/app/modules/donnations/donnations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DomSanitizer, SafeResourceUrl, Title} from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-donnations',
@@ -10,14 +11,15 @@ export class DonnationsComponent implements OnInit {
 
   public url;
 
-  constructor(private titleService:Title, private sanitizer: DomSanitizer) {
-    this.titleService.setTitle('Over2Craft - Faire un don');
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://financement.over2craft.com/'
-      + encodeURI(window.location.hash.substr(1))
-    );
+  constructor(private titleService:Title, private sanitizer: DomSanitizer, @Inject(DOCUMENT) private document: Document) {
+    // this.titleService.setTitle('Over2Craft - Faire un don');
+    // this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://financement.over2craft.com/'
+    //   + encodeURI(window.location.hash.substr(1))
+    // );
   }
 
   ngOnInit(): void {
+    this.document.location.href = 'http://financement.over2craft.com';
   }
 
 }
