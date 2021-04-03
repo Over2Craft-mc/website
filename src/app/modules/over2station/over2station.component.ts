d16d9d4273c67dfbd3be904c538b62959286748e
Custo <shyndard@gmail.com>
2021-04-04 01:57:33 +0200

    Add listener count

--- src/app/modules/over2station/over2station.component.ts
+++ src/app/modules/over2station/over2station.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 import { Over2stationService } from 'src/app/services/over2station/over2station.service';
 
 @Component({
@@ -6,17 +7,28 @@ import { Over2stationService } from 'src/app/services/over2station/over2station.
   templateUrl: './over2station.component.html',
   styleUrls: ['./over2station.component.css']
 })
-export class Over2stationComponent implements OnInit {
+export class Over2stationComponent implements OnInit, OnDestroy {
 
   public status: any;
   public currentId: string;
+  private intervalSub: Subscription;
 
-  constructor(private o2sService: Over2stationService, private elementRef: ElementRef) { 
+  constructor(private o2sService: Over2stationService, private elementRef: ElementRef) {
     this.currentId = Math.random().toString(36).substring(8);
   }
 
-
   ngOnInit(): void {
+    this.loadStatus();
+    this.intervalSub = interval(15000).subscribe(() => {
+      this.loadStatus();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSub.unsubscribe();
+  }
+
+  loadStatus(): void {
     this.o2sService.getStatus().subscribe(data => {
       if (data && data.icestats) {
         this.status = data.icestats;
